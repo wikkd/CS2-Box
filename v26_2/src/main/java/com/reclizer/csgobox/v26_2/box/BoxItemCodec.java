f3379f36c8370e0be5bee60de7d5104057c36a1a
wikkd <18630220656@qq.com>
2026-08-09 16:11:53 +0800

    feat: add /csbox nbt hand (v26_2)

--- v26_2/src/main/java/com/reclizer/csgobox/v26_2/box/BoxItemCodec.java
+++ v26_2/src/main/java/com/reclizer/csgobox/v26_2/box/BoxItemCodec.java
@@ -20,7 +20,7 @@ import net.minecraft.world.item.ItemStack;
  * <p>Both directions tolerate the legacy "tag" NBT string format and the
  * modern "components" data-component patch format.</p>
  */
-final class BoxItemCodec {
+public final class BoxItemCodec {
 
     private static final Gson GSON = new Gson();
 
@@ -109,7 +109,7 @@ final class BoxItemCodec {
         }
     }
 
-    static JsonObject serializeItemStack(ItemStack stack) {
+    public static JsonObject serializeItemStack(ItemStack stack) {
         JsonObject obj = new JsonObject();
         Identifier itemId = BuiltInRegistries.ITEM.getKey(stack.getItem());
         obj.addProperty("id", itemId.toString());
